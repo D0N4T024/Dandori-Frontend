a28812b9df328eeb72c387dbda525706d4e42d90
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2024-11-02 22:37:49 -0400

    Creating terms and conditions component

---
+++ dandori/src/components/TermsAndConditions.jsx
@@ -0,0 +1,48 @@
+import Typography from '@mui/material/Typography';
+
+export default function TermsAndConditions() {
+  return (
+    <div>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Términos y Condiciones de Uso de Dandori
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        Bienvenido a Dandori. Al acceder y utilizar nuestro sitio web y servicios, aceptas cumplir con los siguientes <b>Términos y Condiciones</b>. Por favor, léelos detenidamente antes de utilizar la plataforma. Si no estás de acuerdo con estos términos, te recomendamos no utilizar Dandori.<br/><br/>
+        
+        <b>1. Definiciones</b><br/>
+        Dandori: Se refiere a la plataforma web que proporciona herramientas para comparar precios y datos nutricionales de alimentos en diversos supermercados.<br/>
+        Usuario: Cualquier persona que accede a la plataforma Dandori.<br/>
+        Contenido: Toda la información, datos, textos, gráficos, imágenes y otros materiales disponibles en Dandori.<br/><br/>
+
+        <b>2. Uso del Sitio Web</b><br/>
+        Dandori se proporciona para uso personal y no comercial. Los usuarios no deben:<br/>
+        - Reproducir, duplicar, copiar, vender o explotar cualquier parte del contenido sin el consentimiento expreso de Dandori.<br/>
+        - Utilizar la plataforma para cualquier actividad ilegal o no autorizada.<br/>
+        - Interferir o intentar interferir con el funcionamiento adecuado de la plataforma.<br/><br/>
+
+        <b>3. Propiedad Intelectual</b><br/>
+        Todo el contenido presente en Dandori, incluidos textos, gráficos, logotipos, iconos, imágenes y software, es propiedad de Dandori o de sus proveedores de contenido y está protegido por las leyes de propiedad intelectual. El uso no autorizado del contenido puede violar los derechos de autor, marcas registradas y otras leyes.<br/><br/>
+
+        <b>4. Privacidad</b><br/>
+        Dandori se compromete a proteger la privacidad de sus usuarios. La información personal recopilada a través de la plataforma se maneja de acuerdo con nuestra Política de Privacidad.<br/><br/>
+
+        <b>5. Exclusión de Responsabilidad</b><br/>
+        Dandori se esfuerza por mantener la precisión de la información proporcionada, pero no garantiza que el contenido sea completo, exacto o actualizado. La plataforma no se hace responsable de:<br/>
+        - Cualquier error u omisión en el contenido.<br/>
+        - Cualquier daño directo, indirecto, incidental o consecuente que resulte del uso o la imposibilidad de uso de la plataforma.<br/><br/>
+
+        <b>6. Enlaces a Terceros</b><br/>
+        Dandori puede contener enlaces a sitios web de terceros. Estos enlaces se proporcionan solo para conveniencia del usuario y no implican aprobación por parte de Dandori. No nos hacemos responsables del contenido o prácticas de privacidad de estos sitios web.<br/><br/>
+
+        <b>7. Modificaciones de los Términos</b><br/>
+        Dandori se reserva el derecho de modificar estos Términos y Condiciones en cualquier momento. Cualquier cambio se notificará a los usuarios mediante la actualización de la fecha en la parte superior de esta página. Es responsabilidad del usuario revisar estos términos periódicamente para estar al tanto de cualquier modificación.<br/><br/>
+
+        <b>8. Jurisdicción y Ley Aplicable</b><br/>
+        Estos Términos y Condiciones se rigen por las leyes de [país], y cualquier disputa relacionada con ellos estará sujeta a la jurisdicción exclusiva de los tribunales de República Dominicana.<br/><br/>
+
+        <b>9. Contacto</b><br/>
+        Para cualquier consulta o comentario sobre estos Términos y Condiciones, puedes ponerte en contacto con nosotros a través de servicio@intec.edu.do.
+      </Typography>
+    </div>
+  );
+}
