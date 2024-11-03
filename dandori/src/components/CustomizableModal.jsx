33e016d877b04c427976ba296cf8ef9279db8144
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2024-11-02 21:55:57 -0400

    Creating styles for the customizable modal component

---
+++ dandori/src/components/CustomizableModal.jsx
@@ -0,0 +1,57 @@
+'use client'
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '70vw',
+  maxHeight: '80vh',
+  borderRadius: '25px',
+  overflowY: 'hidden',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+//   p: 2,
+};
+
+
+export default function CustomizableModal({ content, textTag }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+        <div onClick={handleOpen}>
+            {textTag()}
+        </div>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <button onClick={handleClose} className='iconButton' style={{position: 'fixed', top: '5px', right: '5px'}}><CloseIcon/></button>
+                <div 
+                    style={{
+                        maxHeight: '75vh',
+                        overflowY: 'auto',
+                        marginTop: '3em',
+                        marginLeft: '2em',
+                        paddingBottom: '2em',
+                        paddingRight: '1em',
+                        marginRight: '0.5em',
+                    }}
+                >
+                    {content()}
+                </div>
+            </Box>
+        </Modal>
+    </div>
+  );
+}
