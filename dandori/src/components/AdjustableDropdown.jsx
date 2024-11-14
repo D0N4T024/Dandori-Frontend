31875126ea8383f3ed7405c6a44a09403f813aab
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2024-11-13 22:12:48 -0400

    Creating ajustable dropdown omponent

---
+++ dandori/src/components/AdjustableDropdown.jsx
@@ -0,0 +1,44 @@
+
+'use client';
+import styles from "./AdjustableDropdown.module.css"
+import { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
+export default function AdjustableDropdown ({ value:value, options:options }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(value);
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        setIsOpen(false);
+    };
+    
+    return(
+        <div className={styles.dropdown}>
+            <div className={styles.quantityDropdown} onClick={toggleDropdown}>
+                <h5>{selectedOption}</h5>
+                {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </div>
+            {isOpen && (
+                <ClickAwayListener onClickAway={toggleDropdown}>
+                    <ul className={styles.dropdownMenu}>
+                        {options.map((option, index) => (
+                            <li
+                                key={index}
+                                className={styles.dropdownOption}
+                                onClick={() => handleOptionClick(option)}
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </ul>
+                </ClickAwayListener>
+                
+            )}
+        </div>
+    )
+}
