b3ae1da2fe5324a8b352a43cedb860bd13512cce
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2024-11-20 23:50:11 -0400

    Automatizating product description

--- dandori/src/app/(main)/store/[StoreId]/[ProductId]/page.jsx
+++ dandori/src/app/(main)/store/[StoreId]/[ProductId]/page.jsx
@@ -63,7 +63,7 @@ export default function Product() {
                         className={styles.topImage}/>
                 <div className={styles.topDescriptionOptions}>
                     <div>
-                        <p style={{ fontWeight: "300" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p style={{ fontWeight: "300" }}>{product.description}</p>
                     </div> 
                     <div className={styles.topOptions}>
                         <div className={styles.topCartOptions}>
@@ -84,6 +84,9 @@ export default function Product() {
                 <div className={styles.CostTrendChart}>
                     <CostTrendChart tendency={product.tendency}/>
                 </div>
+
+
+
             </div>
         </div>
     )
